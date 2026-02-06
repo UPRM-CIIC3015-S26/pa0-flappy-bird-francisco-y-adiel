81b6a40f28127b510d0aba899c927c854742d45e
franciscovalentin10 <francisco.valentin10@upr.edu>
2026-02-06 11:04:39 -0400

    todo 1 todo 2.1 todo 2.2 todo 4 (ya esta funcionando falta perfeccionar) FYVO

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ some of the game mechanics.
 '''
 # Setup the screen -->
 screen = pygame.display.set_mode((400, 600))
-pygame.display.set_caption("Flappy Bird")
+pygame.display.set_caption("Flappy Birds")
 
 # Colors -->
 # NOTE: This is in the RGB (Red, Green, Blue) format
@@ -34,28 +34,28 @@ score_y = 10
 
 # Player Variables -->
 bird_x = 50
-bird_y = 300
+bird_y = 200
 bird_velocity = 0
 # TODO 1: Tweaking the physics
 # Looks like the player is falling too quickly not giving a change to flap it's wing, maybe tweak around with the value of this variable
-gravity = 9.81
-jump = -10
+gravity = 0.5
+jump = -8
 # Pipe Variables -->
-pipe_x = 400
-pipe_width = 70
+pipe_x = 200
+pipe_width = 40
 # TODO 2.1: A Little gap Problem
 # You probably noticed when running the code that it's impossible the player to go through the gaps
 # play around with the pipe_gap variable so that its big enough for the player to pass through
-pipe_gap = 20
+pipe_gap = 110
 pipe_height = random.randint(100, 400)
 # TODO 2.2: The too fast problem
 # The pipes are moving way too fast! Play around with the pipe_speed variable until you find a good
 # speed for the player to play in!
-pipe_speed = 20
+pipe_speed = 7
 
-score = 0
+score = 1
 game_over = False
-game_started = False
+game_started = True
 
 clock = pygame.time.Clock()
 
@@ -80,7 +80,7 @@ while running:
                     # It is your job to find why this is happening! (Hint: What variable stores the y coordinates
                     # of the bird)
                     bird_velocity = 0
-                    pipe_x = 400
+                    pipe_x = 700
                     score = 0
                     game_over = False
                     game_started = True
@@ -97,7 +97,7 @@ while running:
             # TODO 4: Fixing the scoring
             # When you pass through the pipes the score should be updated to the current score + 1. Implement the
             # logic to accomplish this scoring system.
-            score = 1
+            score += 1
 
         if bird_y > 600 or bird_y < 0:
             game_over = True
